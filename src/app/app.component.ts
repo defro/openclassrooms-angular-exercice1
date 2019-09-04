e3f06bec6aab54eddbf6835229df5205c12bf793
Joël Gaujard <j.gaujard@gmail.com>
2019-09-05 00:31:47 +0200

    Finilaze exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'exercice1';
+  posts = [
+    {
+      title: 'Post neutre',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus urna sed facilisis euismod.',
+      loveIts: 0,
+      created_at: new Date('2010-04-24T03:30:00')
+    },
+    {
+      title: 'Post adoré',
+      content: 'Vestibulum laoreet varius nibh vel hendrerit. Fusce feugiat fermentum ipsum, posuere ultrices sem cursus eget.',
+      loveIts: 2,
+      created_at: new Date('2019-03-23T20:49:00')
+    },
+    {
+      title: 'Post detesté',
+      content: 'Nunc tempor ante sit amet sapien pharetra, non varius lectus faucibus. Aliquam cursus elit non nulla consequat pulvinar.',
+      loveIts: -2,
+      created_at: new Date()
+    }
+  ];
 }
